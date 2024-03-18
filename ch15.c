e3e077235231c250dcfcda057949f4f6d1b96061
msharipov <msharipovr@gmail.com>
2024-03-18 01:12:29 -0400

    started ch15

---
+++ ch15.c
@@ -0,0 +1,15 @@
+/*
+*   TODO:
+*   - search for a word
+*   - replace a word
+*   - regex matching
+*   - regex search in a string
+*   - query-replace with regex
+*   - extend query-replace with grouping
+*/
+
+
+int
+main() {
+
+}
