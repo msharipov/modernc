4d2e8f63dec91c7965d751c9673ffdb2a61c8df5
Maxim Sharipov <msharipovr@gmail.com>
2024-04-03 22:40:43 -0400

    ch1: refactor/reformat

--- ch1.c
+++ ch1.c
@@ -6,30 +6,36 @@
 #include <string.h>
 #include <stdint.h>
 
+#define TEST_ARRAY_SIZE 15
+
 void
-print_array(const double * const arr, const size_t n) {
+print_array(const size_t n, const double arr[static n]) {
     
     for (size_t i = 0; i < n; i++) {
+
         printf("[%zu]: %f\n", i, arr[i]);
     }
 }
 
 
 void
-fill_rand(double * const arr, const size_t n, const unsigned seed) {
+fill_rand(const size_t n, double arr[static n], const unsigned seed) {
     
     srand(seed);
     for (size_t i = 0; i < n; i++) {
+            
         arr[i] = (double)(rand()) / RAND_MAX;
     }
 }
 
 
 bool
-is_sorted(const double * arr, const size_t n) {
+is_sorted(const size_t n, const double arr[static n]) {
 
     for (size_t i = 0; i < n - 1; i++) {
+
         if (arr[i] > arr[i + 1]) {
+
             return false;
         }
     }
@@ -39,7 +45,7 @@ is_sorted(const double * arr, const size_t n) {
 
 
 void
-swap(double * const arr, const size_t a, const size_t b) {
+swap(double arr[static 1], const size_t a, const size_t b) {
 
     const double temp = arr[a];
     arr[a] = arr[b];
@@ -48,12 +54,15 @@ swap(double * const arr, const size_t a, const size_t b) {
 
 
 void
-quick_sort(double * const arr, const size_t len) {
+quick_sort(const size_t len, double arr[static len]) {
     
     if (len < 3) {
+            
         if (len == 2 && arr[0] > arr[1]) {
+            
             swap(arr, 0, 1);
         }
+            
         return;
     }
     
@@ -65,19 +74,24 @@ quick_sort(double * const arr, const size_t len) {
     for (;;) {
 
         while (arr[i] < P && i < len) {
+
             i++;
         }
 
         while (arr[j] > P && j >= i) {
+            
             j--;
         }
 
         if (i < j) {
+
             swap(arr, i, j);
+            
         } else {
+
             swap(arr, 0, j);
-            quick_sort(arr, j);
-            quick_sort(&arr[i], len - i);
+            quick_sort(j, arr);
+            quick_sort(len - i, &arr[i]);
             return;
         }
 
@@ -87,22 +101,26 @@ quick_sort(double * const arr, const size_t len) {
 
 
 void
-merge_sort(double * const arr, const size_t len) {
+merge_sort(const size_t len, double arr[static len]) {
     
     if (len < 3) {
+
         if (len == 2 && arr[0] > arr[1]) {
+
             swap(arr, 0, 1);
         }
+
         return;
     }
     
     size_t middle = len / 2 ;
-    merge_sort(arr, middle);
-    merge_sort(&arr[middle], len - middle);
+    merge_sort(middle, arr);
+    merge_sort(len - middle, &arr[middle]);
 
     double * temp = calloc(len, sizeof(double));
 
     if (!temp) {
+
         printf("Memory error!\n");
         exit(1);
     }
@@ -147,35 +165,44 @@ merge_sort(double * const arr, const size_t len) {
 
 
 int
-main(const int argc, const char * argv[]) {
+main(const int argc, const char * argv[static argc]) {
     
     uint16_t seed = 1;
     
     if (argc == 2) {
-        char seed_str[15];
-        strncpy(seed_str, argv[1], 14);
+
+        char seed_str[TEST_ARRAY_SIZE];
+        strncpy(seed_str, argv[1], TEST_ARRAY_SIZE - 1);
         char * end = NULL;
         seed = strtoul(seed_str, &end, 10) % UINT16_MAX;
     }
 
-    double arr[15] = {0};
-    fill_rand(arr, 15, seed);
-    quick_sort(arr, 15);
-    print_array(arr, 15);
-    if (is_sorted(arr, 15)) {
+    double arr[TEST_ARRAY_SIZE] = {0};
+    fill_rand(TEST_ARRAY_SIZE, arr, seed);
+    quick_sort(TEST_ARRAY_SIZE, arr);
+    print_array(TEST_ARRAY_SIZE, arr);
+
+    if (is_sorted(TEST_ARRAY_SIZE, arr)) {
+
         printf("Sorted correctly!\n");
+
     } else {
+
         printf("Not sorted correctly!\n");
     }
 
-    fill_rand(arr, 15, seed + 27);
-    merge_sort(arr, 15);
-    print_array(arr, 15);
-    if (is_sorted(arr, 15)) {
+    fill_rand(TEST_ARRAY_SIZE, arr, seed + 27);
+    merge_sort(TEST_ARRAY_SIZE, arr);
+    print_array(TEST_ARRAY_SIZE, arr);
+
+    if (is_sorted(TEST_ARRAY_SIZE, arr)) {
+
         printf("Sorted correctly!\n");
+
     } else {
+
         printf("Not sorted correctly!\n");
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
