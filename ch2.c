ffb82d73dc23bcc2be6ca3fa7657cd70c2d6da1d
Maxim Sharipov <msharipovr@gmail.com>
2024-04-04 20:26:05 -0400

    ch2: refactor/reformat

--- ch2.c
+++ ch2.c
@@ -3,12 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-enum Num_Deriv_Error{
-    
+typedef enum Num_Deriv_Error{
     SUCCESS = 0,
     UNDEFINED = 1,
     UNSTABLE = 2,
-};
+} Num_Deriv_Error;
 
 typedef struct Num_Deriv_Result Num_Deriv_Result;
 struct Num_Deriv_Result{
@@ -28,13 +27,9 @@ num_deriv(double(*f)(double), const double x, const double dx) {
     const double f4 = f(x + h);
     const double f5 = f(x + 2*h);
     
-    Num_Deriv_Result result = {0, SUCCESS};
+    Num_Deriv_Result result = {.value = 0, .error = SUCCESS};
 
-    if (fpclassify(f1) == FP_NAN || fpclassify(f1) == FP_INFINITE ||
-        fpclassify(f2) == FP_NAN || fpclassify(f2) == FP_INFINITE ||
-        fpclassify(f3) == FP_NAN || fpclassify(f3) == FP_INFINITE ||
-        fpclassify(f4) == FP_NAN || fpclassify(f4) == FP_INFINITE ||
-        fpclassify(f5) == FP_NAN || fpclassify(f5) == FP_INFINITE) {
+    if (isnan(f3) || isinf(f3)) {
 
         result.error = UNDEFINED;
         return result;
@@ -43,7 +38,7 @@ num_deriv(double(*f)(double), const double x, const double dx) {
     const double dfdx = (f1 - 8*f2 + 8*f4 - f5) / (12*h);
     result.value = dfdx;
     
-    if (fpclassify(dfdx) == FP_NAN || fpclassify(dfdx) == FP_INFINITE) {
+    if (isnan(dfdx) || isinf(dfdx)) {
 
         result.error = UNDEFINED;
         return result;
@@ -53,6 +48,7 @@ num_deriv(double(*f)(double), const double x, const double dx) {
     const bool neg_monotonic = (f1 > f2 && f2 > f3 && f3 > f4 && f4 > f5);
 
     if (pos_monotonic || neg_monotonic) { 
+
         return result;
     }
     
@@ -114,57 +110,80 @@ int
 main() {
 
     const double EPS = 1e-9;
-    Num_Deriv_Result res;
-    const char * err_txt[] = {"Solution is valid.",
-                              "Derivative does not exist.",
-                              "Derivative is unstable."};
+    Num_Deriv_Result res = {0};
+    const char* err_txt[] = {
+        [SUCCESS] = "Solution is valid.",
+        [UNDEFINED] = "Derivative does not exist.",
+        [UNSTABLE] = "Derivative is unstable."
+    };
 
     res = num_deriv(testf1, 2.0, EPS);
     if (!res.error) {
+
         printf("d/dx (x) @ 2.0 = %f\n", res.value);
+
     } else {
+
         printf("Cannot compute the derivative! %s\n", err_txt[res.error]);
     }
     
     res = num_deriv(testf2, 0.0, EPS);
     if (!res.error) {
+
         printf("d/dx (x^2) @ 0.0 = %f\n", res.value);
+
     } else {
+
         printf("Cannot compute the derivative! %s\n", err_txt[res.error]);
     }
 
     res = num_deriv(testf2, -1.0, EPS);
     if (!res.error) {
+        
         printf("d/dx (x^2) @ -1.0 = %f\n", res.value);
+        
     } else {
+
         printf("Cannot compute the derivative! %s\n", err_txt[res.error]);
     }
 
     res = num_deriv(testf2, 5.0e-10, EPS);
     if (!res.error) {
+        
         printf("d/dx (x^2) @ 5.0e-10 = %f\n", res.value);
+        
     } else {
+        
         printf("Cannot compute the derivative! %s\n", err_txt[res.error]);
     }
     
     res = num_deriv(testf3, 3.0e-9, EPS);
     if (!res.error) {
+        
         printf("d/dx (cos(1/x)) @ 3.0e-9 = %f\n", res.value);
+        
     } else {
+        
         printf("Cannot compute the derivative! %s\n", err_txt[res.error]);
     }
     
     res = num_deriv(testf3, 1.0/3.141592654, EPS);
     if (!res.error) {
+        
         printf("d/dx (cos(1/x)) @ 1/pi = %f\n", res.value);
+
     } else {
+
         printf("Cannot compute the derivative! %s\n", err_txt[res.error]);
     }
 
     res = num_deriv(testf4, 0.0, EPS);
     if (!res.error) {
+
         printf("d/dx (x^0.333) @ 0.0 = %f\n", res.value);
+
     } else {
+
         printf("Cannot compute the derivative! %s\n", err_txt[res.error]);
     }
 
